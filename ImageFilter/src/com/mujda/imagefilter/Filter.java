2bf739cb51c34a3581831c47cda5f119429b6b4d
msabbasi <msabbasi@ualberta.ca>
2015-02-07 22:10:42 -0700

    Working Lab1 project

--- ImageFilter/src/com/mujda/imagefilter/Filter.java
+++ ImageFilter/src/com/mujda/imagefilter/Filter.java
@@ -1,5 +1,7 @@
 package com.mujda.imagefilter;
 
+import java.util.Arrays;
+
 import android.graphics.Bitmap;
 import android.util.Log;
 
@@ -9,8 +11,6 @@ public class Filter {
 		
 		int width = image.getWidth();
 		int height = image.getHeight();
-		System.out.println(height);
-		System.out.println(width);
 		int[] pixels = new int[height*width];
 		image.getPixels(pixels, 0, width, 0, 0, width, height);
 		
@@ -28,8 +28,6 @@ public class Filter {
 		    }
 		}
 		
-		System.out.println("done first");
-		
 		for (int y = 0; y < height; y++) {    
 		    for (int x = 0; x < width; x++) {
 		        index = y * width + x;
@@ -38,27 +36,32 @@ public class Filter {
 		        bsum=0;
 		        gsum=0;
 		        
-		        for (int y1=y-size/2; y1<=y+size/2; y++) {
-		        	for (int x1=x-size/2; x1<=x+size/2; x++) {
-		        		System.out.println(y1);
-		        		try {
-		        			index1 = y1 * width + x1;
-		        			rsum += r[index1];
-		        			bsum += b[index1];
-		        			gsum += g[index1];
-		        		}
-		        		catch (IndexOutOfBoundsException e) {
-		        			break;
-		        		}
+		        for (int y1=y-size/2; y1<=y+size/2; y1++) {
+		        	if (y1<0) {
+		        		continue;
+		        	}
+		        	if (y1>height-1) {
+		        		break;
+		        	}
+		        	for (int x1=x-size/2; x1<=x+size/2; x1++) {
+		        		if (x1<0) {
+			        		continue;
+			        	}
+			        	if (x1>width-1) {
+			        		break;
+			        	}
+			        	//System.out.println(y1 + " " + x1 + " " + index);
+			        	index1 = y1 * width + x1;
+			        	rsum += r[index1];
+			        	bsum += b[index1];
+			        	gsum += g[index1];
 		        	}
 		        }
 		        
 		        pixels[index] = 0xff000000 | ((rsum/(size*size)) << 16) | ((gsum/(size*size)) << 8) | (bsum/(size*size));
 		    }
 		}
-		
-		System.out.println("done second");
-		
+				
 		Bitmap newImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 		newImage.setPixels(pixels, 0, width, 0, 0, width, height); 	
 
@@ -68,5 +71,73 @@ public class Filter {
 		return newImage;
 	}
 	
+	public static Bitmap applyMedianFilter(Bitmap image, int size) {
+		
+		int width = image.getWidth();
+		int height = image.getHeight();
+		int[] pixels = new int[height*width];
+		image.getPixels(pixels, 0, width, 0, 0, width, height);
+		
+		int i, index, index1;
+		int[] array = new int[size*size];
+		int[] resultant = new int[height*width];
+		
+		for (int y = 0; y < height; y++) {    
+		    for (int x = 0; x < width; x++) {
+		        index = y * width + x;
+		        
+		        i=0;
+		        
+		        for(int j=0;j<size*size;j++){
+		        	array[j]=0;
+		        }
+		        
+		        for (int y1=y-size/2; y1<=y+size/2; y1++) {
+		        	if (y1<0) {
+		        		continue;
+		        	}
+		        	if (y1>height-1) {
+		        		break;
+		        	}
+		        	for (int x1=x-size/2; x1<=x+size/2; x1++) {
+		        		if (x1<0) {
+			        		continue;
+			        	}
+			        	if (x1>width-1) {
+			        		break;
+			        	}
+			        	//System.out.println(y1 + " " + x1 + " " + index);
+			        	index1 = y1 * width + x1;
+			        	array[i]=pixels[index1];
+			        	i++;
+		        	}
+		        }
+		        int median;
+		        Arrays.sort(array);
+		        int middle = array.length-i+(i)/2;
+		        if(i%2==0){
+		        	median=(array[middle]+array[middle-1])/2;
+		        }
+		        else {
+		        	median=array[middle+1];
+		        }
+		        
+		        
+		        resultant[index] = median;
+		    }
+		}
+				
+		Bitmap newImage = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		newImage.setPixels(resultant, 0, width, 0, 0, width, height); 	
+
+		if (null != image) {
+		    image.recycle();
+		}
+		return newImage;
+	}
 	
+	
+
 }
+
+
